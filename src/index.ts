6f7015658ca68dd27e52f17efd2614858d0fbe87
marcoadaza <marcoadaza@gmail.com>
2025-03-08 12:30:12 -0500

    test

--- src/index.ts
+++ src/index.ts
@@ -11,5 +11,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', router); // Use the router for your routes
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on r http://localhost:${PORT}`);
 });
